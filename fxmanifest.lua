d2f211d9830e6f2d9af7ad9aebc8423f138055a9
GamercraftStudios <58826382+BurnhamR@users.noreply.github.com>
2023-12-18 09:28:00 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,18 +1,8 @@
-------------------------------------------------------------------------
-------------------------------------------------------------------------
---			DO NOT EDIT IF YOU DON'T KNOW WHAT YOU'RE DOING			  --
---     							 									  --
---	   For support join my discord: https://discord.gg/Z9Mxu72zZ6	  --
-------------------------------------------------------------------------
-------------------------------------------------------------------------
-
-version "1.0.0"
+version "1.0.1"
 description "HeliScript"
 author "Andyyy#7666"
-
 fx_version 'bodacious'
 game 'gta5'
-
 client_scripts {
     "client.lua",
 }
